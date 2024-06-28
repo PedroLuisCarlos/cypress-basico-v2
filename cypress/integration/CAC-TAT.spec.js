2e3cd470cea5f97ca24115bd982c9df696f388aa
Pedro Carlo <114144418+PedroLuisCarlos@users.noreply.github.com>
2024-06-28 17:43:35 +0100

    Update CAC-TAT.spec.js

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -103,18 +103,63 @@ it('preenche e limpa os campos nome, sobrenome, email e telefone', function() {
       .should('have.value', 'feedback')
     })
 
-    it.only('marca cada tipo de atendimento', function(){
+    it('marca cada tipo de atendimento', function(){
       cy.get('input[type="radio"]')
-      .should('have.length', 3)
+      .should('have.length', 3) 
       .each(function($radio) {
         cy.wrap($radio).check()
         cy.wrap($radio).should('be.checked')
       })
-
+    })
+    it('marca ambos checkboxes, depois desmarca o último', function(){
+      cy.get('input[type="checkbox"]')
+      .check()
+      .should('be.checked')
+      .last()
+      .uncheck()
+      .should('not.be.checked')
     })
 
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function(){
+      cy.get('#firstName').type('Pedro')
+    cy.get('#lastName').type('Carlos')
+    cy.get('#email').type('pedrovilhalva@gmail.com')
+    cy.get('#phone-checkbox').check()
+    cy.get('#open-text-area').type('Teste')
+    cy.contains('button','Enviar').click()
+    cy.get('.error').should('be.visible')
+})
+    it('seleciona um arquivo da pasta fixtures)', function(){
+      cy.get('input[type="file"')
+      .should('not.have.value')
+      .selectFile('./cypress/fixtures/example.json')
+      .should(function($input) { 
+        expect($input[0].files[0].name).to.equal('example.json')
+      })
+    })
+
+    it('seleciona um arquivo simulando um drag-and-drop', function(){
+      cy.get('input[type="file"')
+      .should('not.have.value')
+      .selectFile('./cypress/fixtures/example.json', {action:'drag-drop'})
+      .should(function($input) { 
+        expect($input[0].files[0].name).to.equal('example.json')
+      })
+    })
+
+    it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function(){
+      cy.fixture('example.json').as('samplefile')
+      cy.get('input[type="file"')
+      .selectFile('@samplefile')
+      .should(function($input) { 
+        expect($input[0].files[0].name).to.equal('example.json')
+      })
+    })
+
+  })
 
 
-    } )
+
+
   
   
